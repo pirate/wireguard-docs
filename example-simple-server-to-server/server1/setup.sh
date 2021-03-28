1d3916e36af76d07ed039d59f307d39b5e2722eb
Salvydas Lukosius <sall@w-ss.io>
2021-03-28 17:13:24 +0100

    Fixed rest missing values

--- example-simple-server-to-server/server1/setup.sh
+++ example-simple-server-to-server/server1/setup.sh
@@ -6,8 +6,8 @@ apt update
 apt install wireguard
 
 # to enable kernel relaying/forwarding ability on bounce servers
-echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
-echo "net.ipv4.conf.all.proxy_arp = 1" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward = 1" >>/etc/sysctl.conf
+echo "net.ipv4.conf.all.proxy_arp = 1" >>/etc/sysctl.conf
 sysctl -p /etc/sysctl.conf
 
 # to add iptables forwarding rules on bounce servers
